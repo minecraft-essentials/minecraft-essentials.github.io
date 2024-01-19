9a3d843b1e5b9452049b76b0cf9b664ade7ff754
eveeify <eveeg1971@gmail.com>
2024-01-20 00:32:22 +1100

    fix: formating

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
 import starlightPlugin from "@astrojs/starlight-tailwind";
 
-
-const accent = { 200: '#a8d7a6', 600: '#008318', 900: '#003f07', 950: '#002d04' };
-const gray = { 100: '#f7f6f3', 200: '#f0ede8', 300: '#c5c1bb', 400: '#918b7f', 500: '#5d574c', 700: '#3d372d', 800: '#2b261c', 900: '#1a1813' };
-
+const accent = {
+	200: "#a8d7a6",
+	600: "#008318",
+	900: "#003f07",
+	950: "#002d04",
+};
+const gray = {
+	100: "#f7f6f3",
+	200: "#f0ede8",
+	300: "#c5c1bb",
+	400: "#918b7f",
+	500: "#5d574c",
+	700: "#3d372d",
+	800: "#2b261c",
+	900: "#1a1813",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -15,5 +27,3 @@ export default {
 	},
 	plugins: [starlightPlugin()],
 };
-
-
