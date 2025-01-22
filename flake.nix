1443eedf72e8ba2c8acd7bf45820f37f467ba6df
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-01-23 04:58:16 +1100

    chore: switch off of devenv and use mkshell
    The reason was because of bloat of a devshell uneeded packages plus just
    general bloat

--- flake.nix
+++ flake.nix
@@ -1,59 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
-  outputs =
-    {
-      self,
-      nixpkgs,
-      devenv,
-      systems,
-      ...
-    }@inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          nodejs-packages = with pkgs.nodePackages; [
-            vscode-langservers-extracted
-            typescript-language-server
-          ];
-        in
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        pkgs,
+        config,
+        lib,
+        ...
+      }: 
+      {
+        devShells.default = pkgs.mkShell
         {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                dotenv.enable = true;
-                packages = with pkgs; [ ] ++ nodejs-packages;
-                languages = {
-                  javascript = {
-                    enable = true;
-                    bun = {
-                      enable = true;
-                      install.enable = true;
-                    };
-                  };
-                  typescript = {
-                    enable = true;
-                  };
-                };
-              }
-            ];
-          };
-        }
-      );
+          packages = with pkgs; [
+            typescript-language-server 
+						typescript
+						dprint
+						bun
+          ];
+        };
+      };
     };
 }
